06fb3010feed836506e4a475766d7168bee5ce43
Pol Ramos <pol.rm2004@gmail.com>
2023-08-09 20:46:00 +0200

    ex01

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 11:19:32 by eralonso          #+#    #+#             */
-/*   Updated: 2023/07/02 11:44:48 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/08/09 20:45:37 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@ int	main( void )
 	std::string	str = "HI THIS IS BRAIN";
 	std::string	*stringPTR = &str;
 	std::string &stringREF = str;
+
+	std::cout << "str address" << std::setw(9) << ": "<< &str << std::endl;
+	std::cout << "stringPTR address" << std::setw(3) << ": "<< stringPTR << std::endl;
+	std::cout << "stringREF address" << std::setw(3) << ": "<< &stringREF << std::endl;
+	std::cout << "str string" << std::setw(10) << ": "<< str << std::endl;
+	std::cout << "stringPTR string" << std::setw(4) << ": "<< *stringPTR << std::endl;
+	std::cout << "stringREF string" << std::setw(4) << ": "<< stringREF << std::endl;
 
 	return (0);
 }
